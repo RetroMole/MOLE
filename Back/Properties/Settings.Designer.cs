6c8db7eb54bc27a1b5b50f56a69a7365d1898ce9
Vawlpe <vawlpe@gmail.com>
2020-10-14 21:09:29 +0300

    Fix Settings and make sure no duplicates of DLLs get uploaded from Back and Win project dirs but are copied to output dir properly

--- Back/Properties/Settings.Designer.cs
+++ Back/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace MOLE_Back.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.6.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.7.0.0")]
     public sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -23,12 +23,51 @@ namespace MOLE_Back.Properties {
             }
         }
         
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
-        public bool AllowDangerAddress {
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool UPDATE_main {
             get {
-                return ((bool)(this["AllowDangerAddress"]));
+                return ((bool)(this["UPDATE_main"]));
+            }
+            set {
+                this["UPDATE_main"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("release")]
+        public string UPDATE_main_mode {
+            get {
+                return ((string)(this["UPDATE_main_mode"]));
+            }
+            set {
+                this["UPDATE_main_mode"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool UPDATE_asar {
+            get {
+                return ((bool)(this["UPDATE_asar"]));
+            }
+            set {
+                this["UPDATE_asar"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("release")]
+        public string UPDATE_asar_mode {
+            get {
+                return ((string)(this["UPDATE_asar_mode"]));
+            }
+            set {
+                this["UPDATE_asar_mode"] = value;
             }
         }
     }
