42d3fb2cfde298786d632674da52966113c7821b
Vawlpe <vawlpe@gmail.com>
2021-01-06 09:13:40 +0200

    Remove win ROM-Xplore

--- src/Win/GFX-View.cs
+++ src/Win/GFX-View.cs
@@ -148,7 +148,7 @@ namespace win
 
         private void panel2_Paint(object sender, PaintEventArgs e)
         {
-            /*
+           
             float pxsize = 14.56f;
             Graphics g = e.Graphics;
             g.ScaleTransform(pxsize, pxsize);
@@ -156,7 +156,7 @@ namespace win
 
             Color[] pal = BuildPal();
             DrawSpr(new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,20,21,22,23,24,25,26,27,28,29,30,31 }, pal, g);
-            */
+            
        }
     }
 }
