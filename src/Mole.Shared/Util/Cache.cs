bb745e28e69f63d40bc7966ee8388979b96f6f85
Vawlpe <vawlpe@gmail.com>
2021-11-02 00:33:51 +0200

    New Cache class i should've committed earlier

---
+++ src/Mole.Shared/Util/Cache.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+
+namespace Mole.Shared.Util
+{
+    public static partial class Cache
+    {
+        public enum Type
+        {
+            Graphics,
+            PointerTable
+        }
+        public static void SaveCache(Type type, byte[] info, byte[] data, string name)
+        {
+            if (info is null) info = new byte[14];
+            using (var fs = File.OpenWrite($"{name}.cache.bin"))
+            {
+                fs.WriteByte((byte)(((ushort)type >> 8) & 0xFF));
+                fs.WriteByte((byte)((ushort)type & 0xFF));
+                fs.Write(info);
+                fs.Write(data);
+            }
+                SaveHash($"{name}.cache.bin.sha1");
+        }
+        public static bool TryLoadCache(string name, out object data)
+        {
+            bool hash = CheckHash($"{name}.cache.bin.sha1");
+            if (hash) data = LoadCache($"{name}.cache.bin"); else data = null;
+            return hash;
+        }
+        public static byte[] LoadCache(string path) => File.ReadAllBytes(path);
+        public static void SaveHash(string path) => File.WriteAllBytes(path, new SHA1Managed().ComputeHash(File.ReadAllBytes(path)));
+        public static bool CheckHash(string path) => File.ReadAllBytes(path) == new SHA1Managed().ComputeHash(File.ReadAllBytes(path));
+    }
+}
