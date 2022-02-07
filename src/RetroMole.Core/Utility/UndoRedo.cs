a15b1aac7703968390ddd49f5ef37683075864ce
Vawlpe <vawlpe@gmail.com>
2022-02-07 12:09:59 +0200

    Rename & basic Undo/Redo system, this closes #31

---
+++ src/RetroMole.Core/Utility/UndoRedo.cs
@@ -0,0 +1,47 @@
+﻿namespace RetroMole.Core.Utility
+{
+    public abstract class Command
+    {
+        public abstract void Execute();
+        public abstract void UnExecute();
+    }
+    public class UndoRedo
+    {
+        public Stack<Command> UndoCommands = new();
+        public Stack<Command> RedoCommands = new();
+        public void Redo(int levels)
+        {
+            for (int i = 1; i <= levels; i++)
+            {
+                if (RedoCommands.Count != 0)
+                {
+                    Command command = RedoCommands.Pop();
+                    command.Execute();
+                    UndoCommands.Push(command);
+                }
+
+            }
+        }
+
+        public void Undo(int levels)
+        {
+            for (int i = 1; i <= levels; i++)
+            {
+                if (UndoCommands.Count == 0)
+                    continue;
+
+                Command command = UndoCommands.Pop();
+                command.UnExecute();
+                RedoCommands.Push(command);
+
+            }
+        }
+
+        public void Do(Command cmd)
+        {
+            cmd.Execute();
+            UndoCommands.Push(cmd);
+            RedoCommands.Clear();
+        }
+    }
+}
