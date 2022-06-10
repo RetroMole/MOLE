439a9055e5ea80bb278f0bca7f724dd92f1b02ff
Vawlpe <vawlpe@gmail.com>
2022-06-10 21:07:42 +0300

    Linq go brrrrrrr
    (dont do this at home kids)

--- src/RetroMole.Launch/Program.cs
+++ src/RetroMole.Launch/Program.cs
@@ -3,23 +3,16 @@ using System.Reflection;
 namespace RetroMole;
 public static class Launch
 {
-	public static Core.Interfaces.Package[] pckgs;
 	public static void Main(string[] args)
 	{
 		var pckgsDir = Path.Combine(Core.Utility.CommonDirectories.Exec, "Packages");
-
-		var pckgAsms = Directory.GetFileSystemEntries(pckgsDir, "*.dll", SearchOption.AllDirectories);
-		var compressedPckgsFiles = Directory.GetFileSystemEntries(pckgsDir, "*.mole.package", SearchOption.AllDirectories);
-
-		List<Core.Interfaces.Package> pckgList = new();
-		
-		foreach (var f in pckgAsms)
-			pckgList.AddRange(Core.Utility.Import.AssemblyPackages(Assembly.LoadFrom(f)));
-
-		foreach (var f in compressedPckgsFiles)
-			pckgList.AddRange(Core.Utility.Import.CompressedPackages(f));
-
-		pckgs = pckgList.ToArray();
+		var pckgs = Directory.GetFileSystemEntries(pckgsDir, "*.dll", SearchOption.AllDirectories)
+			.Concat(Directory.GetFileSystemEntries(pckgsDir, "*.mole.package", SearchOption.AllDirectories))
+			.SelectMany(p => 
+					p.EndsWith(".dll")          ? Core.Utility.Import.AssemblyPackages(Assembly.LoadFrom(p))
+			   	  : p.EndsWith(".mole.package") ? Core.Utility.Import.CompressedPackages(p)
+			   	  : throw new Exception($"Uhh... how did you.. BAD PACKAGE @ {p}"))
+			.ToArray();
 
 		pckgs.First().Controllers.First().Main(() => {
 			Gui.Main.TestUI();
